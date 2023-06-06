993035846fa07a678cfa292f5186bae219057a0d
HASH-002 <koserhasan002@gmail.com>
2023-06-06 22:00:11 +0530

    Learn Props

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,27 +1,46 @@
 import './App.css';
 
 function App() {
-  const name = <h1>react</h1>;
   return (
-    // <div className="App">
-    //   {name}{name} // u called a variable/ function
-    // </div>
-    <GetNameComponent /> // u called a component
+    <div>
+      {/* strings with "" and number with braces ... !!! quotes never goes with { } */}
+      <GetNameComponent name="react" email="react@facebook.com" phone={8482935565} />
+      <GetNameComponent /> {/*Wont display any info as no props are passed but wont show error too*/}
+
+      <JobComponent name="react" salary={90000} company="Facebook" />
+    </div>
   );
 }
 
-// Javascript function
-// Return anything or null
-const GetName = () => {
-  return "react";
-};
 
 // React component
 // Always return UI stuff
 // !!! Must start with capital letter otherwise file wont be compiled
-const GetNameComponent = () => {
-  return <h1>react</h1>;
+// Properties sent by parent must be perceived as props
+
+/*
+Props are perceived as object
+const props = {
+  name: "react",
+  email: "react@facebook.com",
+  phone: 8482935565
+};
+*/
+
+const GetNameComponent = (props) => {
+  return (<>
+    <h1>{props.name}</h1>
+    <h1>{props.email}</h1>
+    <h1>{props.phone}</h1>
+  </>
+  );
 };
 
-
+const JobComponent = (props) => {
+  return (<>
+    <div>{props.name}</div>
+    <div>{props.salary}</div>
+    <div>{props.company}</div>
+  </>);
+};
 export default App;
