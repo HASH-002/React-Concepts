6944f42bfbc6b8d539f195302962179649ad2204
HASH-002 <koserhasan002@gmail.com>
2023-06-06 21:38:33 +0530

    Learn JSX

---
+++ learn-react/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  const name = <h1>react</h1>;
+  return (
+    <div className="App">
+      {name}{name}
+    </div>
+  );
+}
+
+// Javascript function
+// Return anything or null
+const GetName = () => {
+  return "react";
+};
+
+// React component
+// Always return UI stuff
+// !!! Must start with capital letter otherwise file wont be compiled
+const GetNameComponent = () => {
+  return <h1>react</h1>;
+};
+
+
+export default App;
