f8b46647b389148a6ccaf6bc71154e905eb368da
HASH-002 <koserhasan002@gmail.com>
2023-06-08 19:48:50 +0530

    Learn Axios Ex-3

--- learn-react-3/src/App.js
+++ learn-react-3/src/App.js
@@ -3,22 +3,23 @@ import { useState } from "react";
 import Axios from "axios";
 
 function App() {
-  const [name, setName] = useState("");
-  const [info, setInfo] = useState(null);
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
 
-  const getInfo = () => {
-    Axios.get(`https://api.agify.io?name=${name}`).then((res) => {
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
       console.log(res.data);
-      setInfo(res.data);
+      setGeneratedExcuse(res.data[0].excuse);
     });
   };
 
   return (
-    <div div className="App" >
-      <input onChange={(e) => setName(e.target.value)} />
-      <button onClick={getInfo}> Predict Age </button>
-      {/* Amazaing syntax in js if variable is not null then only access it property == variable?.property */}
-      <p>Name: {info?.name} Age: {info?.age}</p>
+    <div className="App">
+      <h1> Generate An Excuse </h1>
+      <button onClick={() => fetchExcuse("party")}> Party</button>
+      <button onClick={() => fetchExcuse("family")}> Family</button>
+      <button onClick={() => fetchExcuse("office")}> Office </button>
+
+      <p> {generatedExcuse} </p>
     </div>
   );
 }
